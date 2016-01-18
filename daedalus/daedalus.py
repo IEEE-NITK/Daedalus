44211e81fafb06d2826530ad3ac2df6e531db436
chinmaydd <chinmay1dd@gmail.com>
2016-01-18 23:16:33 +0530

    Adds daedalus class

--- daedalus/daedalus.py
+++ daedalus/daedalus.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python
 
+class Daedalus():
+	def __init__(self):
+		pass
 
-def dummy():
-    print "Time to start!"
+	def loadpubkey(self, args, type=None):
+		if type=='file':
+			try:
+				f = open(args, 'r')
+			except:
+				print "File not found"
+		else:
+			# Assuming input is of the form (n,e,)
+			self.n = args[0]
+			self.e = args[1]
+
+	def loadprivkey(self, args, type=None):
+		if type=='file':
+			try:
+				f = open(args, 'r')
+			except:
+				print "File not found"
+		else:
+			# Assuming input is of the form (d,)
+			self.d = args[0]
+
 
 def shell():
 	entered = ''
 	while entered != 'quit':
 		entered = raw_input('>> ')
+		exec(entered)
 
 
 if __name__ == '__main__':

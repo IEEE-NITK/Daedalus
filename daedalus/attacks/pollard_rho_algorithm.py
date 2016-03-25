a11796acf5c818f896977fff96fbd15c6ed2bf63
CB Yuvaraj <yuvarajchinthapulusu22@gmail.com>
2016-03-26 00:03:54 +0530

    Update pollard_rho_algorithm.py
    updated pr

--- daedalus/attacks/pollard_rho_algorithm.py
+++ daedalus/attacks/pollard_rho_algorithm.py
@@ -2,21 +2,13 @@
 
 import random
 import math
-from Crypto.PublicKey import RSA
-
-errors = []
 
 def gcd(a,b):
-    if a is 0:
+    if a<=0:
         return b
     return gcd(b%a,a)
 
 def pollard_rho(number,x,y):
-    try:
-        number = int(number)
-    except TypeError as n:
-        globals()['errors'].append(n)
-
     d = 1
     while d is 1:
         x = (x**2+1)%number
@@ -29,21 +21,21 @@ def pollard_rho(number,x,y):
         d = gcd(z,number)
     return d
 
-
-def attack(input={},errors = {},results = {}):
-    try :
-          number = int(input["key"])
-    except TypeError as n:
-        globals()['errors'].append(n)
-
+def pollard_rho_attack(input={}):
+    #initialising x and y values
     x=2
     y=2
-    factor = pollard_rho(number,x,y)
-    while factor is number or 1:
+    errors=[]
+    try:
+        number = input['n']
+    except TypeError as e:
+        errors.append(e)
+    factor1 = pollard_rho(number,x,y)
+    while factor1 is  1:
         x = x+1
         y = y+1
-        factor = pollard_rho(number,x,y)
-    factor2 = int(number/factor)
-    results["p "]= factor
-    results["q "]= factor2
-    return {'errors': globals()['errors'],'results':results}
+        factor1 = pollard_rho(number,x,y)
+    factor2 = int(number/factor1)
+    results ={'p':factor1,'q':factor2}
+    return {'errors':errors,'results':results}
+

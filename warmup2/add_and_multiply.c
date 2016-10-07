47570826f437b8e142a5a1e1a6f44fb66fdada1f
PrabhanjanPM <prmannari@gmail.com>
2016-10-07 16:13:26 +0530

    Added multiplication as a separate function

--- warmup2/add_and_multiply.c
+++ warmup2/add_and_multiply.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int* multiply(int* n1, int c1, int* n2, int c2);
 int* add(int* n1, int c1, int* n2, int c2);
+int count_digits(int number);
 int main(int argc, char** argv){
 	if(argc!=3){
 		printf("Usage- %s <number1> <number2>\n",argv[0]);
@@ -30,6 +32,7 @@ int main(int argc, char** argv){
 	
 	//Storing and printing the numbers
 	int n1[c1],n2[c2],i;
+	printf("Number1- \t");
 	for(i=0;i<c1;i++){
 		if(i>=c1-d1){
 			char c;
@@ -42,6 +45,7 @@ int main(int argc, char** argv){
 		}
 	}
 	printf("\n");
+	printf("Number2- \t");
 	for(i=0;i<c2;i++){
 		if(i>=c2-d2){
 			char c;
@@ -67,33 +71,36 @@ int main(int argc, char** argv){
 		n[i]=sum%10;
 		n[i-1]=sum/10;
 	}
+	printf("Sum-\t");
 	for(i=0;i<c;i++){
 		printf("%d",n[i]);
 	}
 	printf("\n");
-	
-	//Multiplying the numbers 
-	int j,k,l=0;
+	int *n3=multiply(n1,c1,n2,c2);
 	c=c1+c2;
-	int product[c];
+	printf("Product-\t");
 	for(i=0;i<c;i++){
-		product[i]=0;
-	}
-	for(i=c1-1;i>=0;i--,l++){
-		for(j=c2-1,k=c-l-1;j>=0;j--,k--){
-			int sum=product[k]+n1[i]*n2[j];
-			product[k]=sum%10;
-			product[k-1]+=sum/10;
-		}
-	}
-	for(i=0;i<c;i++){
-		printf("%d",product[i]);
+		printf("%d",*(n3+i));
 	}
 	printf("\n");
-
-	//Implement muliplication using gauss trick 
-	//
-	//
-	
 	return 0;
+}
+int* multiply(int* n1, int c1, int* n2, int c2){
+	int c3, *product;
+	c3=c1+c2;
+	product=(int *)malloc(sizeof(int)*c3);
+	int i,j,k,l=0;
+	for(i=0;i<c3;i++){
+		*(product+i)=0;
+	}
+	for(i=c1-1;i>=0;i--,l++){
+		for(j=c2-1,k=c3-l-1;j>=0;j--,k--){
+			int p1=*(n1+i);
+			int p2=*(n2+j);
+			int sum=*(product+k)+p1*p2;
+			*(product+k)=sum%10;
+			*(product+k-1)=*(product+k-1)+sum/10;
+		}
+	}
+	return product;
 }
